38abe5c386628bbae0bb361a997beb61ab351714
Kevin James <KevinJames@thekev.in>
2023-08-01 18:10:14 +0100

    feat(chase): initial chase importers
    Based on:
    https://github.com/mtlynch/beancount-chase-bank

--- beancount_importer/__init__.py
+++ beancount_importer/__init__.py
@@ -1,11 +1,15 @@
 import importlib
 
 from .amex import AmexImporter
+from .chase_checking import ChaseCheckingImporter
+from .chase_credit import ChaseCreditImporter
 from .revolut import RevolutImporter
 
 
 __version__ = importlib.metadata.version('beancount-importer')
 __all__ = [
     'AmexImporter',
+    'ChaseCheckingImporter',
+    'ChaseCreditImporter',
     'RevolutImporter',
 ]

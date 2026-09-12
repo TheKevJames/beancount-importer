ab4b873b0cbb64aad6f33cd2fdaf15c98165195f
Kevin James <KevinJames@thekev.in>
2026-09-12 12:44:53 +0100

    chore(deps): unpin max requires-python

--- beancount_importer/__init__.py
+++ beancount_importer/__init__.py
@@ -1,17 +1,18 @@
+# flake8: noqa: IMR241
 import importlib.metadata
 
-from .activobank import ActivobankImporter  # noqa: IMR241
-from .amex import AmexImporter  # noqa: IMR241
-from .brim import BrimImporter  # noqa: IMR241
-from .chase import ChaseImporter  # noqa: IMR241
-from .eq import EqImporter  # noqa: IMR241
-from .milleniumbcp import MilleniumbcpImporter  # noqa: IMR241
-from .paypal import PaypalImporter  # noqa: IMR241
-from .rbc import RbcImporter  # noqa: IMR241
-from .remitbee import RemitbeeImporter  # noqa: IMR241
-from .revolut import RevolutImporter  # noqa: IMR241
-from .tangerine import TangerineImporter  # noqa: IMR241
-from .wealthsimple import WealthsimpleImporter  # noqa: IMR241
+from .activobank import ActivobankImporter
+from .amex import AmexImporter
+from .brim import BrimImporter
+from .chase import ChaseImporter
+from .eq import EqImporter
+from .milleniumbcp import MilleniumbcpImporter
+from .paypal import PaypalImporter
+from .rbc import RbcImporter
+from .remitbee import RemitbeeImporter
+from .revolut import RevolutImporter
+from .tangerine import TangerineImporter
+from .wealthsimple import WealthsimpleImporter
 
 __version__ = importlib.metadata.version('beancount-importer')
 __all__ = [
